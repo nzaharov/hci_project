8fcd593ba0e9c2ebb8a196c82cdcbfa2761894a8
Yavor Petkov <yavorp.98@gmail.com>
2020-05-30 15:36:19 +0300

    Simple login form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext} from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import Login from './login/index';
+import { ThemeContext } from './theme-context'
 
 function App() {
+  const theme = useContext(ThemeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Route path='/' component={Login}/>
+      <Route path='/home' component={Login} />
+    </ThemeProvider>
+    </BrowserRouter>
+    
+  )
 }
 
 export default App;

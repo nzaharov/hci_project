723382c091369f573e37a5ef12f365378d145083
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-05-30 21:16:03 +0300

    docs

---
+++ docs/precache-manifest.8a6ba9134f3727c5fb466126bada3b12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6495d3336d30e437421017607cb0cc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6fbb12ee44a26df4a6",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "6e3247b34dbeda0e70fe",
+    "url": "/static/css/main.b764a926.chunk.css"
+  },
+  {
+    "revision": "5c6fbb12ee44a26df4a6",
+    "url": "/static/js/2.168f298c.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/static/js/2.168f298c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3247b34dbeda0e70fe",
+    "url": "/static/js/main.cfbdcaff.chunk.js"
+  },
+  {
+    "revision": "0f8fa8c0dc24cbec0987",
+    "url": "/static/js/runtime-main.e9261261.js"
+  }
+]);
